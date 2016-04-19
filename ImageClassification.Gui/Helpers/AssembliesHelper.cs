7d34366013bfce27d39027375242395619c2ca5e
Szymon Bartnik <szymbar099@student.polsl.pl>
2016-04-19 20:30:27 +0200

    Foldered libraries in Release output.

---
+++ ImageClassification.Gui/Helpers/AssembliesHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Wkiro.ImageClassification.Gui.Helpers
+{
+    public static class AssembliesHelpers
+    {
+        private static readonly string[] DirNames =
+        {
+            "Libraries",
+        };
+
+        public static Dictionary<string, string> LoadKnownAssemblies()
+        {
+            var toReturn = new Dictionary<string, string>();
+
+            foreach (var enumerateFile in DirNames.SelectMany(x => Directory.GetFiles(x, "*.dll", SearchOption.AllDirectories)))
+            {
+                try
+                {
+                    toReturn.Add(AssemblyName.GetAssemblyName(enumerateFile).FullName, Path.GetFullPath(enumerateFile));
+                }
+                catch { /* ignore native files */ }
+            }
+
+            return toReturn;
+        }
+
+        public static Assembly Resolve(IReadOnlyDictionary<string, string> knownAssemblies, object s, ResolveEventArgs e)
+        {
+            var requiredAssembly = new AssemblyName(e.Name);
+            var assembly =
+                AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .FirstOrDefault(x => new AssemblyName(x.FullName).Name == requiredAssembly.Name);
+
+            if (assembly != null)
+                return assembly;
+
+            var binding = knownAssemblies.SingleOrDefault(x => x.Key.StartsWith(e.Name.Split(',')[0]));
+            return Assembly.LoadFile(binding.Value);
+        }
+    }
+}
---
+++ ImageClassification.Gui/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Wkiro.ImageClassification.Gui.Helpers;
+
+namespace Wkiro.ImageClassification.Gui
+{
+    public class Startup
+    {
+        private static readonly Dictionary<string, string> KnownAssemblies;
+
+        static Startup()
+        {
+            // Assemblies lack resolve
+            KnownAssemblies = AssembliesHelpers.LoadKnownAssemblies();
+            AppDomain.CurrentDomain.AssemblyResolve += (s, e) => AssembliesHelpers.Resolve(KnownAssemblies, s, e);
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
