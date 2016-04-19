3ab5206a0a3144a24743f24ac64107fe306457c7
Szymon Bartnik <szymbar099@student.polsl.pl>
2016-04-19 19:33:48 +0200

    GUI changes.

--- ImageClassification.Gui/Views/MainWindowView.xaml.cs
+++ ImageClassification.Gui/Views/MainWindowView.xaml.cs
@@ -1,15 +1,20 @@
-﻿using System.Windows;
+﻿using System;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using MahApps.Metro.Controls;
 using Wkiro.ImageClassification.Gui.ViewModels;
 
 namespace Wkiro.ImageClassification.Gui.Views
 {
-    public partial class MainWindowView : Window
+    public partial class MainWindowView : MetroWindow
     {
         public MainWindowView(MainWindowViewModel viewModel)
         {
             InitializeComponent();
             DataContext = viewModel;
+
+            var iconUri = new Uri("pack://application:,,,/ImageClassification.Gui;component/Resources/TitleBarIcon.ico", UriKind.RelativeOrAbsolute);
+            Icon = BitmapFrame.Create(iconUri);
         }
 
         private void OnLogTextBoxTextChanged(object sender, TextChangedEventArgs e)
